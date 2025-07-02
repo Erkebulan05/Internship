7b42a15ec5b17e82732cbf78c0ea3a599f55968c
Erkebulan <135618798+Erkebulan05@users.noreply.github.com>
2025-07-02 07:33:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
-import psycopg2
+from fastapi import FastAPI, Request
+from bot.router import router_messages
+from admin.admin_router import router as admin_router
+from config.clients import get_client_by_token
+from bot.telegram import send_telegram_message
+from urllib.parse import unquote
 
 
-conn = psycopg2.connect(
-    dbname="ReservationByAPI",
-    user="your_username",
-    password="your_password",
-    host="localhost",
-    port="5432"
-)
+
+app = FastAPI()
+app.include_router(admin_router)
+
+@app.post("/wenbook/telegram")
+async def telegram_wenbook(request: Request):
+    data = await request.json()
+    return await router_messages(data, platform="telegram" )
+
+@app.post("/wenbook/whatsapp")
+async def whatsapp_wenbook(request: Request):
+    data = await request.json()
+    return await router_messages(data , platform="whatsapp")
 
 
-with conn.cursor() as cur:
-    cur.execute("""
-        CREATE TABLE IF NOT EXISTS reservations (
-            id SERIAL PRIMARY KEY,
-            name TEXT NOT NULL,
-            date DATE NOT NULL,
-            time TIME NOT NULL,
-            guests INTEGER NOT NULL
-        );
-    """)
-    conn.commit()
+@app.post("/wenbook/instagram")
+async def instagram_wenbook(request: Request):
+    data = await request.json()
+    return await router_messages(data , platform="instagram")
